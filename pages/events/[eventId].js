93ddbe37085fff67d21bf2b0509d3141153aa136
Reza Zeraat <rezacr588@gmail.com>
2021-08-20 22:59:09 +0430

    add event detail component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,26 @@
+import { Fragment } from "react"
+import EventSummary from "../../components/event-detail/event-summary"
+import EventLogistics from "../../components/event-detail/event-logistics"
+import EventContent from "../../components/event-detail/event-content"
+import { useRouter } from "next/router"
+import {getEventById} from "../../dummy-data"
+
 function EventDetailPage() {
-  return <div>
-    <h1>
-      Event Detail Page
-    </h1>
-  </div>
+  const router = useRouter()
+  const event = getEventById(router.query.eventId)
+  if(!event) return <p>No Event Found</p>
+  return <Fragment>
+    <EventSummary title={event.title} />
+    <EventLogistics
+      date={event.date}
+      address={event.location}
+      image={event.image}
+      imageAlt={event.title}
+    />
+    <EventContent>
+      <p>{event.description}</p>
+    </EventContent>
+  </Fragment>
 }
 
 export default EventDetailPage
